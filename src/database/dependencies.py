71740b38d68e2b263307b5328e79784d07c680e0
Taynara <taaymoraais@gmail.com>
2024-04-17 19:44:31 -0300

    Inserir informações no banco de dados

---
+++ src/database/dependencies.py
@@ -0,0 +1,18 @@
+import cv2
+
+from matplotlib import pyplot as plt
+
+import pytesseract
+
+import numpy as np
+
+import math
+
+from datetime import date
+
+from PIL import Image
+
+from dateutil.relativedelta import relativedelta
+
+"""class created"""
+#from filter import Filter
