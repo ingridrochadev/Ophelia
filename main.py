cf35c492e5672b099a37b79b8355be0130c6e2eb
bianca <biablackthorn@gamil.com>
2024-04-19 21:39:43 -0300

    interface init and organization

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
-
+import streamlit as st
 sys.path.append("./src")  # Adiciona o diretório pai ao caminho do sistema
 
 from pages import login
 # from pages import register_user
 
-login.tela_login()
+#login.tela_login()
 # register_user.tela_register()
+st.title("Teste")
