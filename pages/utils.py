d64c9602fb943627e2f8835da6ff78153dde816e
yasmim <yasmim.ferreira.200583@gmail.com>
2024-04-20 18:14:47 -0300

    add alterar/redefinir de senha + verificar email

--- pages/utils.py
+++ pages/utils.py
@@ -6,10 +6,6 @@ import bcrypt
 load_dotenv()
 
 class Sistema:
-    def __init__(self):
-        self.conn = self.estabelecer_conexao()
-        self.cur = self.conn.cursor()
-    
     def estabelecer_conexao(self):
         conn = psycopg2.connect(
             dbname=getenv("DATABASE_NAME"),
@@ -20,15 +16,18 @@ class Sistema:
         conn.autocommit = False  # Desativa o modo de autocommit para fazer commits manuais
         return conn
     
+    
     def encriptar_senha(self, senha):
         salt = bcrypt.gensalt()
         hash_senha = bcrypt.hashpw(senha.encode('utf-8'), salt) # Gera o hash da senha usando o salt
         return salt.decode('utf-8') + hash_senha.decode('utf-8') # Concatena o salt e o hash e retorna str
     
     
-    def criar_usuario(self, nome: str, cpf: str, email: str, senha: str, matricula: int):
+    def criar_usuario(self, nome: str, cpf: str, email: str, senha: str, matricula: str):
         if nome and cpf and email and senha and matricula:
             try:
+                self.conn = self.estabelecer_conexao(self)
+                self.cur = self.conn.cursor()
                 hashed_senha = self.encriptar_senha(senha)
                 
                 self.cur.execute("INSERT INTO usuarios (nome, cpf, email, senha, matricula) VALUES (%s, %s, %s, %s, %s)", (nome, cpf, email, hashed_senha, matricula))
@@ -37,23 +36,33 @@ class Sistema:
             except Exception as error:
                 print(f"Houve um erro ao inserir usuário. Motivo: {error}")
                 self.conn.rollback()
+                
+            finally:
+                self.cur.close()
+                self.conn.close()
         else:
             print("Preencha todos os campos obrigatórios!")
             
         
-    def excluir_usuario(self, matricula: int):
+    def excluir_usuario(self, matricula: str):
         if matricula:
             try:
+                self.conn = self.estabelecer_conexao(self)
+                self.cur = self.conn.cursor()
+                
                 self.cur.execute("DELETE FROM usuarios WHERE matricula = %s", [matricula])
                 self.conn.commit()
 
             except Exception as error:
                 print(f"Houve um erro ao excluir usuário. Motivo: {error}")
                 self.conn.rollback()
+                
+            finally:
+                self.cur.close()
+                self.conn.close()
         else:
             print("Preencha todos os campos obrigatórios!")
     
-    # ** Métodos não finalizados / testados
     
     def fazer_login(self, email: str, senha: str):
         if email and senha:
@@ -73,7 +82,7 @@ class Sistema:
                     if senha_str == salt + hash_fornecido.decode('utf-8'):
                         print("Login realizado com sucesso! Redirecionando...")
                         return True
-                    else:
+                    else: 
                         print("E-mail e/ou senha incorreta. Tente novamente!")
                         return False
                 else:
@@ -82,7 +91,86 @@ class Sistema:
             except Exception as error:
                 print(f"Ocorreu um erro na tentativa de login. Motivo: {error}")
                 return False
+            
+            finally:
+                self.cur.close()
+                self.conn.close()
                 
         else:
             print("Preencha todos os campos obrigatórios.")
             return False
+            
+            
+    def alterar_senha(self, email: str, senha_armazenada: str, nova_senha: str, nova_senha2: str):
+        if email and senha_armazenada and nova_senha and nova_senha2:
+            try:
+                self.conn = self.estabelecer_conexao(self)
+                self.cur = self.conn.cursor()
+                
+                # Verifica se o email está cadastrado
+                self.cur.execute("SELECT senha FROM usuarios WHERE email = %s", (email, ))
+                usuario = self.cur.fetchone()
+                if usuario:
+                    senha_str = usuario[0] # Recebe a senha do banco de dados
+                    salt = senha_str[:29] # Extrai o salt do hash armazenado
+                    hash_fornecido = bcrypt.hashpw(senha_armazenada.encode('utf-8'), salt.encode('utf-8')) # Gera o hash da senha inserida utilizando o mesmo salt
+                    
+                    # Verifica se o hash gerado corresponde ao hash armazenado
+                    if senha_str == salt + hash_fornecido.decode('utf-8'):
+                        if nova_senha == nova_senha2:
+                            # Senhas correspondem, parte pra alteração
+                            hashed_senha = self.encriptar_senha(self, nova_senha)
+                            self.cur.execute("UPDATE usuarios SET senha = %s WHERE email = %s", (hashed_senha, email))
+                            self.conn.commit()
+                            print("Senha alterada com sucesso!")
+                        else:
+                            print("As senhas não correspondem. Tente novamente!")
+                            
+                    else: 
+                        print("E-mail e/ou senha incorreta. Tente novamente!")
+                        
+                else:
+                    print("E-mail e/ou senha incorreta. Tente novamente!")
+                
+            except Exception as error:
+                print(f'Ocorreu um erro ao alterar a senha: {error}')
+                self.conn.rollback()
+                
+            finally:
+                self.cur.close()
+                self.conn.close()
+        else:
+            print("Preencha todos os campos obrigatórios!")
+            
+            
+    def redefinir_senha(self, email, nova_senha, nova_senha2):
+        if email and nova_senha and nova_senha2:
+            try:
+                self.conn = self.estabelecer_conexao(self)
+                self.cur = self.conn.cursor()
+                
+                # Verifica se o email está cadastrado
+                self.cur.execute("SELECT senha FROM usuarios WHERE email = %s", (email, ))
+                usuario = self.cur.fetchone()
+                if usuario:
+                    if nova_senha == nova_senha2:
+                        # Senhas correspondem, parte pra alteração
+                        hashed_senha = self.encriptar_senha(self, nova_senha)
+                        self.cur.execute("UPDATE usuarios SET senha = %s WHERE email = %s", (hashed_senha, email))
+                        self.conn.commit()
+                        print("Senha alterada com sucesso!")
+                    else:
+                        print("As senhas não correspondem. Tente novamente!")
+                            
+                else: 
+                    print("E-mail e/ou senha incorreta. Tente novamente!")
+                
+            except Exception as error:
+                print(f'Ocorreu um erro ao redefinir a senha: {error}')
+                self.conn.rollback()
+                
+            finally:
+                self.cur.close()
+                self.conn.close()
+        else:
+            print("Preencha todos os campos obrigatórios!")
